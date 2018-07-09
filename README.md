9cbc737d8c26c9c74ee1e06fdbfc3b06854a154d
sanerersan <sanerersan_cyq@163.com>
2018-07-09 23:00:16 +0800

    修改包地址

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # du
+类linux du的简单实现，命令行参数包括：  
+-dir path : 指明需要遍历的目录  
+-sn : sort by name，缺省为 按大小从小到大排序  
+-r : 反向排序  
+
+使用 golang实现简单信号量  
--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"sort"
-	"fmt"
-	"path/filepath"
-	"log"
-	"io/ioutil"
 	"flag"
-	"du/capslice"
-	duUtils "du/utils"
+	"fmt"
+	"github.com/sanerersan/du/capslice"
+	duUtils "github.com/sanerersan/du/utils"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"sort"
 )
 
-var sortbyName *bool = flag.Bool("sn",false,"sort as name")
-var sortReverse *bool = flag.Bool("r",false,"sort reverse")
-var rootDir *string = flag.String("dir","/","root directory")
+var sortbyName *bool = flag.Bool("sn", false, "sort as name")
+var sortReverse *bool = flag.Bool("r", false, "sort reverse")
+var rootDir *string = flag.String("dir", "/", "root directory")
 
 var wg duUtils.WaitGroupWapper
 var capInfos *capslice.CapSlice
@@ -21,13 +21,13 @@ var sema *duUtils.SemaphoreWarpper
 
 func init() {
 	capInfos = capslice.NewCapSlice()
-	sema,_ = duUtils.NewSemaphore("du_sema",20,0)
+	sema, _ = duUtils.NewSemaphore("du_sema", 20, 0)
 }
 
 func main() {
 	flag.Parse()
 
-	wg.Run(WalkDirList,*rootDir)
+	wg.Run(WalkDirList, *rootDir)
 	wg.Wait()
 
 	sortAndDisplay()
@@ -45,8 +45,8 @@ func sortAndDisplay() {
 	}
 
 	infos := capInfos.GetCaps()
-	for _,info := range infos {
-		fmt.Println(info.Path,duUtils.GetHumanReadableString(info.Cap))
+	for _, info := range infos {
+		fmt.Println(info.Path, duUtils.GetHumanReadableString(info.Cap))
 	}
 }
 
@@ -54,25 +54,25 @@ func WalkDirList(dirPath string) {
 	sema.WaitSync()
 	defer sema.Release()
 
-	entries,err := ioutil.ReadDir(dirPath)
+	entries, err := ioutil.ReadDir(dirPath)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	
-	for _,entry := range entries {
+
+	for _, entry := range entries {
 		entryName := entry.Name()
 		if ("." == entryName) || (".." == entryName) {
 			continue
 		}
-		fullName := filepath.Join(dirPath,entryName)
+		fullName := filepath.Join(dirPath, entryName)
 		if entry.IsDir() {
-			wg.Run(WalkDirList,fullName)
+			wg.Run(WalkDirList, fullName)
 		} else {
-			fci := &capslice.FileCapacityInfo {
-				Name : entry.Name(),
-				Path : fullName,
-				Cap : entry.Size(),
+			fci := &capslice.FileCapacityInfo{
+				Name: entry.Name(),
+				Path: fullName,
+				Cap:  entry.Size(),
 			}
 			capInfos.Append(fci)
 		}
